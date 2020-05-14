94fdfef370b85d48555d335756a05ea30df2f7cd
ACAPP-dev <acapp909@gmail.com>
2020-05-14 15:43:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const testVar = {}
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arrayObject) => {
+  const result = arrayObject.find(recordObject => recordObject.result === "W")
+  if (result) {return result.year}
+}
+
+console.log(superbowlWin(record))
